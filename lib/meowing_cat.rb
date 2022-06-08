88e7dcecbbee5d19e0050100d1c67a052cc368bc
Micaela Frank <micaelalfrank@gmail.com>
2022-06-08 00:26:28 -0400

    commit tree 413fbf446db5a7598bd34dc1d636b92c1b913e5d

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,11 @@
-## code your solution here. 
+class Cat 
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+end
+
+cinnamon_bun = Cat.new
+cinnamon_bun.name = "Cinnamon Bun"
+cinnamon_bun.meow
